6d40910e4016bf6a594598b32eb70e9f3745bc9c
mo0Oonnn <egorklimenko678@gmail.com>
2024-06-26 18:12:21 +0700

    .

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,30 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = false,
+      analyses = {
+        unusedparams = true,
+        assign = true,
+        defers = true,
+        nilfunc = true,
+        nonewvars = true,
+        simplifyrange = true,
+        simplifyslice = true,
+        tests = true,
+        undeclaredname = true,
+      },
+    },
+  },
+}
